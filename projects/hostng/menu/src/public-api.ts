b479a5ca9c754e48a4dcc1b675a2c538dacd4eb5
ivan-drr <ivan.delrr@gmail.com>
2022-05-07 12:15:46 +0200

    relocate ipe

--- projects/hostng/menu/src/public-api.ts
+++ projects/hostng/menu/src/public-api.ts
@@ -3,7 +3,7 @@
  */
 
 export * from './lib/menu.component';
-export * from './lib/pipes/summarize-text.pipe';
+export * from './lib/summarize-text.pipe';
 export * from './lib/product-card/product-card.component';
 export * from './lib/ration-product/ration-product.component';
 export * from './lib/unique-product/unique-product.component';
